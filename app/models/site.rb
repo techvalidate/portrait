ac2ecd73cc7af6e15c0d9f714c1501fa88519678
Jordan McKible <jordan@mckible.com>
2009-06-19 13:11:05 -0700

    Configure Site with paperclip.

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,2 +1,4 @@
 class Site < ActiveRecord::Base
+  has_attached_file :image, :path => ':rails_root/public/sites/:id/:style/:basename.:extension',
+                            :url  => '/sites/:id/:style/:basename.:extension'
 end
