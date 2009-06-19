87d518c13904201717d41dc06e190639bb934038
Jordan McKible <jordan@mckible.com>
2009-06-19 12:24:15 -0700

    Create Site model.

---
+++ db/migrate/20090619190332_create_sites.rb
@@ -0,0 +1,14 @@
+class CreateSites < ActiveRecord::Migration
+  def self.up
+    create_table :sites do |t|
+      t.string   :url, :image_file_name, :image_content_type
+      t.integer  :image_file_size
+      t.datetime :image_updated_at
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :sites
+  end
+end
