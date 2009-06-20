610f5c2149a65a1012872d192569b3a8bd8e8f87
Jordan McKible <jordan@mckible.com>
2009-06-19 17:01:21 -0700

    Unit test format.

--- test/unit/site_test.rb
+++ test/unit/site_test.rb
@@ -3,13 +3,11 @@ require 'test_helper'
 class SiteTest < ActiveSupport::TestCase
   
   test 'validates an url' do
-    site = Site.new
-    assert !site.valid?
+    assert !Site.new.valid?
   end
   
   test 'validate url format' do
-    site = Site.new :url=>'invalid'
-    assert !site.valid?
+    assert !Site.new(:url=>'invalid').valid?
   end
   
 end
