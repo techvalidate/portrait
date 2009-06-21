d8d011e8e5c62cc6d97fda080ef817ca42e09de2
Jordan McKible <jordan@mckible.com>
2009-06-21 16:22:27 -0700

    Respond to xml requests.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -10,10 +10,19 @@ class SitesController < ApplicationController
   def create
     @site = Site.new params[:site]
     @site.save!
-    redirect_to sites_path
+    respond_to do |format|
+      format.html { redirect_to sites_path    }
+      format.xml  { render :xml=>@site.to_xml }
+    end
   rescue ActiveRecord::RecordInvalid
-    @sites = Site.paginate :page=>params[:page]
-    render :index
+    respond_to do |format|
+      format.html do
+        @sites = Site.paginate :page=>params[:page]
+        render :index
+      end
+      format.xml { render :text=>@site.errors.to_xml, :status=>500 }
+    end
+
   end
   
 end
