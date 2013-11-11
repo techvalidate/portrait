ead1e58a31c117e06ec9922b22c44db70b1c5ec7
Andrew Dixon <dixon@atomicbroadcast.net>
2013-11-10 16:25:11 -0800

    Updated SiteController Specs for Devise.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,26 +1,26 @@
 class SitesController < ApplicationController
-  before_filter      :user_required,  :only=>:api
-  skip_before_filter :admin_required, :only=>:api
+  before_filter      :authenticate_user!
+  before_filter      :admin_required, :except => :api
 
   # GET /sites
   def index
     @sites = Site.order('created_at DESC').page params[:page]
     @site  = Site.new
   end
-  
+
   # POST /sites
   def create
-    @site = @current_user.sites.build params.require(:site).permit(:url)
+    @site = current_user.sites.build params.require(:site).permit(:url)
     @site.save!
     redirect_to sites_url
   rescue ActiveRecord::RecordInvalid
     @sites = Site.order('created_at DESC').page params[:page]
     render action: 'index'
   end
-  
+
   # POST /
   def api
-    @site = @current_user.sites.build url: params[:url]
+    @site = current_user.sites.build url: params[:url]
     @site.save!
     render xml: @site.to_xml(only: [:state], methods: [:image_url])
   rescue ActiveRecord::RecordInvalid
--- spec/controllers/sites_controller_spec.rb
+++ spec/controllers/sites_controller_spec.rb
@@ -1,58 +1,79 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe SitesController do
+  let(:user)  { FactoryGirl.create(:user)  }
   let(:admin) { FactoryGirl.create(:user, :admin) }
-  before(:each) { login_as admin }
 
-  it 'handles / with GET' do
-    get :index
-    response.should be_success
+  shared_examples "unauthorized access" do
+    it "disallows / with GET" do
+      get :index
+      response.should_not render_template(:index)
+    end
   end
 
-  it 'handles /sites with valid parameters and POST' do
-    running {
-      post :create, site: {url: 'http://google.com'}
-      assigns(:site).user.should == admin
-      response.should redirect_to(sites_path)
-    }.should change(Site, :count).by(1)
+  context "without signing in" do
+    it_behaves_like "unauthorized access"
   end
 
-  it 'handles /sites with invalid url and POST' do
-    running {
-      post :create, site: {url: 'invalid'}
+  context "with user signed in" do
+    before(:each) { sign_in user }
+
+    it_behaves_like "unauthorized access"
+  end
+
+  context "with admin signed in" do
+    before(:each) { sign_in admin }
+
+    it "allowes / with GET" do
+      get :index
       response.should be_success
-      response.should render_template(:index)
-    }.should_not change(Site, :count)
+    end
+
+    it 'handles /sites with valid parameters and POST' do
+      running {
+        post :create, site: {url: 'http://google.com'}
+        assigns(:site).user.should == admin
+        response.should redirect_to(sites_path)
+      }.should change(Site, :count).by(1)
+    end
+
+    it 'handles /sites with invalid url and POST' do
+      running {
+        post :create, site: {url: 'invalid'}
+        response.should be_success
+        response.should render_template(:index)
+      }.should_not change(Site, :count)
+    end
   end
 
-  it 'handles / with valid parameters and POST' do
-    running {
-      post :api, url: 'http://google.com'
-      assigns(:site).user.should == admin
-      response.should be_success
-      #
-      # This test is brittle and breaks when the code is refactored, etc...
-      # A regex that matches only the critical part of the message would be
-      # more robust.
-      # response.body.should ==  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<site>\n  <state>success</state>\n  <image-url>/sites/2/original/2-full.png</image-url>\n</site>\n"
-      response.body.should =~ /<state>success<\/state>/
-    }.should change(Site, :count).by(1)
-  end
+  context "through the api" do
+    before(:each) { sign_in user}
 
-  it 'handles / with empty url and POST' do
-    running {
-      post :api
-      response.response_code.should == 500
-      response.body.should == "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<errors>\n  <error>Url is invalid</error>\n</errors>\n"
-    }.should_not change(Site, :count)
-  end
+    it 'handles / with valid parameters and POST' do
+      running {
+        post :api, url: 'http://google.com'
+        assigns(:site).user.should == user
+        response.should be_success
+        # checking the entire return string is brittle and will break when code
+        # is refactored or tests are run in a different order. Use a regex to 
+        # validate the important part and leave the rest alone.
+        response.body.should =~  /<state>success<\/state>/
+      }.should change(Site, :count).by(1)
+    end
+    it 'handles / with empty url and POST' do
+      running {
+        post :api
+        response.response_code.should == 500
+        response.body.should == "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<errors>\n  <error>Url is invalid</error>\n</errors>\n"
+      }.should_not change(Site, :count)
+    end
 
-  it 'handles /sites with invalid url and POST' do
-    running {
-      post :api, :url=>'invalid'
-      response.response_code.should == 500
-      response.body.should == "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<errors>\n  <error>Url is invalid</error>\n</errors>\n"
-    }.should_not change(Site, :count)
+    it 'handles /sites with invalid url and POST' do
+      running {
+        post :api, :url=>'invalid'
+        response.response_code.should == 500
+        response.body.should == "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<errors>\n  <error>Url is invalid</error>\n</errors>\n"
+      }.should_not change(Site, :count)
+    end
   end
-
 end
